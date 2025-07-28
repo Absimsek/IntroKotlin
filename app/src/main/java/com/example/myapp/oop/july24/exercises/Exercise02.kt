e963b74541c12a2410cdb224957d2bdab250d047
Absimsek <aliberkaysimsekk@gmail.com>
2025-07-28 10:14:04 +0300

    25july exercises

---
+++ app/src/main/java/com/example/myapp/oop/july24/exercises/Exercise02.kt
@@ -0,0 +1,44 @@
+package com.example.myapp.oop.july24.exercises
+
+// find the angle sum of given margin count
+fun degreeSum(count : Int):Int{
+    var result=180
+    for(i in 4..count){
+        result+=180
+    }
+return result
+}
+
+// calculate the wage by the given number of days and return it
+// max 8 hours per day
+//hourly 10$
+//overwork hourly 20$
+// 160+ hour is overwork
+fun wageCalc(days : Int):Int{
+    var result=0
+var workingHour=0;
+    for (i in 1..days){
+        workingHour += 8
+        if (workingHour<=160){
+            result+=8*10
+        }else{
+            result+=8*20
+        }
+    }
+    return result
+}
+
+// calculate the payment with given internet quota
+//50 gb 100 $
+//1 gb 4$ after 50gb
+
+fun quotaCalc(quota : Int):Int{
+    var result=0
+    for (i in 1..quota){
+        result+=2
+        if (i>50){
+            result+=2
+        }
+    }
+    return result
+}
---
+++ app/src/main/java/com/example/myapp/oop/july24/exercises/Runner02.kt
@@ -0,0 +1,21 @@
+package com.example.myapp.oop.july24.exercises
+
+import java.util.Scanner
+
+fun main() {
+    val input=Scanner(System.`in`)
+
+    println("please enter the side count of shape")
+    val sideCount= input.nextInt()
+    println("the sum of angles is : ${degreeSum(sideCount)}")
+
+
+    println("\nplease enter the total days worked")
+    val daysWorked=input.nextInt()
+    println("\nyour wage this month is: ${wageCalc(daysWorked)}$")
+
+    println("\n please enter the internet you used this month")
+    val quota=input.nextInt()
+    println("\n your payment this month is : ${quotaCalc(quota)}$")
+
+}
